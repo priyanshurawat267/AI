1227c61517f8ecf495c616833488a252de08d0c2
PRIYANSHU RAWAT <priyanshurawat267@gmail.com>
2026-08-16 18:14:18 +0530

    Update 02 Voice Assistant.py

--- PYTHON PROJECT/02 Voice Assistant.py
+++ PYTHON PROJECT/02 Voice Assistant.py
@@ -1,1 +1,12 @@
 
+import win32com.client
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+l = ["Ram", "Raj", "Ajay"]
+
+for name in l:
+    speaker.Speak(f"shoutout to {l, name}")   
+    print(l)
+
+print("The Successfully Executed!")    
