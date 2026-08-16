2bed7b53fd70178a272648158ee7c435ca027081
PRIYANSHU RAWAT <priyanshurawat267@gmail.com>
2026-08-16 17:55:03 +0530

    Implement reminder feature for hydration
    Added a reminder feature that alerts the user to take a break at a specified time.

--- PYTHON PROJECT/main.py
+++ PYTHON PROJECT/main.py
@@ -1,1 +1,22 @@
 
+import time
+import win32com.client 
+from datetime import datetime
+print("Select 1 min next time an present to future:")
+T = input("Enter your start study time (HH:MM):")
+
+print("Reminder set for :", T)
+
+while True:
+
+    current_time = datetime.now().strftime('%H:%M')
+
+    if current_time == T:
+
+        speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+        speaker.Speak('Stop Your Work Please Drink Water')
+
+        break
+
+    time.sleep(1)
