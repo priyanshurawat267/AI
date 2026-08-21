aedf6199671981a0f1ab1ce03cfa0203fe05df34
PRIYANSHU RAWAT <priyanshurawat267@gmail.com>
2026-08-21 14:46:01 +0530

    Add files via upload

---
+++ PYTHON PROJECT/98 Miltiprocessing.py
@@ -0,0 +1,28 @@
+
+import multiprocessing
+import requests
+import os
+
+def downloadFile(url, name):
+    print(f"Started downloading: {name}")
+    response = requests.get(url)
+    with open(f"files/file{name}.jpg", "wb") as f:
+        f.write(response.content)
+    print(f"Finished downloading: {name}")
+if __name__ == "__main__":
+    # Create files folder if it doesn't exist
+    os.makedirs("files", exist_ok=True)
+    url = "https://picsum.photos/2000/3000"
+    process = []
+    # Create 50 processes
+    for i in range(5):
+        p = multiprocessing.Process(
+            target=downloadFile,
+            args=(url, i)
+        )
+        p.start()
+        process.append(p)
+    # Wait for all processes to finish
+    for p in process:
+        p.join()
+    print("Download file successful!")
